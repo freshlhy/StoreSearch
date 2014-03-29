ad96e0f3af357a8eb364ca1b60f40f68910b749d
freshlhy <freshlhy@gmail.com>
2014-03-29 16:18:14 -0700

    Now uses custom cells for search results

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -8,6 +8,10 @@
 
 #import "SearchViewController.h"
 #import "SearchResult.h"
+#import "SearchResultCell.h"
+
+static NSString *const SearchResultCellIdentifier = @"SearchResultCell";
+static NSString *const NothingFoundCellIdentifier = @"NothingFoundCell";
 
 @interface SearchViewController () <UITableViewDataSource, UITableViewDelegate,
                                     UISearchBarDelegate>
@@ -24,6 +28,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
+
+    UINib *cellNib =
+        [UINib nibWithNibName:SearchResultCellIdentifier bundle:nil];
+    [self.tableView registerNib:cellNib
+         forCellReuseIdentifier:SearchResultCellIdentifier];
+    cellNib = [UINib nibWithNibName:NothingFoundCellIdentifier bundle:nil];
+    [self.tableView registerNib:cellNib
+         forCellReuseIdentifier:NothingFoundCellIdentifier];
+
+    self.tableView.rowHeight = 80;
 }
 
 #pragma mark - UITableViewDataSource
@@ -40,23 +54,19 @@
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"SearchResultCell";
-    UITableViewCell *cell =
-        [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell =
-            [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
-                                   reuseIdentifier:CellIdentifier];
-    }
     if ([_searchResults count] == 0) {
-        cell.textLabel.text = @"Nothing found...";
-        cell.detailTextLabel.text = @"";
+        return [tableView
+            dequeueReusableCellWithIdentifier:NothingFoundCellIdentifier
+                                 forIndexPath:indexPath];
     } else {
+        SearchResultCell *cell = (SearchResultCell *)[tableView
+            dequeueReusableCellWithIdentifier:SearchResultCellIdentifier
+                                 forIndexPath:indexPath];
         SearchResult *searchResult = _searchResults[indexPath.row];
-        cell.textLabel.text = searchResult.name;
-        cell.detailTextLabel.text = searchResult.artistName;
+        cell.nameLabel.text = searchResult.name;
+        cell.artistNameLabel.text = searchResult.artistName;
+        return cell;
     }
-    return cell;
 }
 
 #pragma mark - UISearchBarDelegate
@@ -65,7 +75,7 @@
     [searchBar resignFirstResponder];
 
     _searchResults = [NSMutableArray arrayWithCapacity:10];
-    if (![searchBar.text isEqualToString:@"justin bieber"]) {
+    if (![searchBar.text isEqualToString:@"freshlhy"]) {
         for (int i = 0; i < 3; i++) {
             SearchResult *searchResult = [[SearchResult alloc] init];
             searchResult.name =
@@ -82,6 +92,7 @@
 }
 
 #pragma mark - UITableViewDelegate
+
 - (void)tableView:(UITableView *)tableView
     didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
