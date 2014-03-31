a11fc4df3bbbcb1d20d2b5596cb61c1dfd974259
freshlhy <freshlhy@gmail.com>
2014-03-31 02:08:45 -0700

    Downloading the artwork images

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -95,42 +95,8 @@ static NSString *const LoadingCellIdentifier = @"LoadingCell";
             dequeueReusableCellWithIdentifier:SearchResultCellIdentifier
                                  forIndexPath:indexPath];
         SearchResult *searchResult = _searchResults[indexPath.row];
-        cell.nameLabel.text = searchResult.name;
-
-        NSString *artistName = searchResult.artistName;
-        if (artistName == nil) {
-            artistName = @"Unknown";
-        }
-        NSString *kind = [self kindForDisplay:searchResult.kind];
-        cell.artistNameLabel.text =
-            [NSString stringWithFormat:@"%@ (%@)", artistName, kind];
+        [cell configureForSearchResult:searchResult];
         return cell;
-    }
-}
-
-- (NSString *)kindForDisplay:(NSString *)kind {
-    if ([kind isEqualToString:@"album"]) {
-        return @"Album";
-    } else if ([kind isEqualToString:@"audiobook"]) {
-        return @"Audio Book";
-    } else if ([kind isEqualToString:@"book"]) {
-        return @"Book";
-    } else if ([kind isEqualToString:@"ebook"]) {
-        return @"E-Book";
-    } else if ([kind isEqualToString:@"feature-movie"]) {
-        return @"Movie";
-    } else if ([kind isEqualToString:@"music-video"]) {
-        return @"Music Video";
-    } else if ([kind isEqualToString:@"podcast"]) {
-        return @"Podcast";
-    } else if ([kind isEqualToString:@"software"]) {
-        return @"App";
-    } else if ([kind isEqualToString:@"song"]) {
-        return @"Song";
-    } else if ([kind isEqualToString:@"tv-episode"]) {
-        return @"TV Episode";
-    } else {
-        return kind;
     }
 }
 
